733c86981e1cfefbb18f97e879d0558297768e47
gscottqueen <geoffreysqueen@gmail.com>
2022-03-29 10:25:21 -0400

    remove canvas sketch, include mediaPipe

--- src/index.js
+++ src/index.js
@@ -1,161 +1,66 @@
-const canvasSketch = require('canvas-sketch');
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
 
-const video = document.querySelector('video');
-let streamStarted = false;
+function onResults(results) {
+  canvasCtx.save();
+  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  canvasCtx.drawImage(
+    results.segmentationMask, 0, 0,
+    canvasElement.width,
+    canvasElement.height
+  );
 
-// check for device support
-if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-  console.log("Let's get this party started")
+  // Only overwrite existing pixels.
+  canvasCtx.globalCompositeOperation = 'source-out';
+  canvasCtx.fillStyle = '#FFFFFF';
+  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+  // Only overwrite missing pixels.
+  canvasCtx.globalCompositeOperation = 'destination-atop';
+  canvasCtx.drawImage(
+      results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+  canvasCtx.globalCompositeOperation = 'source-over';
+  drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
+                {color: '#000000', lineWidth: 1});
+  drawLandmarks(canvasCtx, results.poseLandmarks,
+                {color: '#000000', lineWidth: 1});
+  drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
+                {color: '#00000', lineWidth: 1});
+  drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
+                {color: '#000000', lineWidth: 1});
+  drawLandmarks(canvasCtx, results.leftHandLandmarks,
+                {color: '#000000', lineWidth: 1});
+  drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
+                {color: '#000000', lineWidth: 1});
+  drawLandmarks(canvasCtx, results.rightHandLandmarks,
+                {color: '#000000', lineWidth: 1});
+  canvasCtx.restore();
 }
 
-// get user permission and access Media Devices object
-async function generateMesh() {
-  console.log("tying again");
+const holistic = new Holistic({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+}});
 
-  const model = await faceLandmarksDetection.load(
-      faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
-  const predictions = await model.estimateFaces({
-    input: document.querySelector("video")
-  });
-
-if (predictions.length > 0) {
-    /*
-    `predictions` is an array of objects describing each detected face, for example:
-
-    [
-      {
-        faceInViewConfidence: 1, // The probability of a face being present.
-        boundingBox: { // The bounding box surrounding the face.
-          topLeft: [232.28, 145.26],
-          bottomRight: [449.75, 308.36],
-        },
-        mesh: [ // The 3D coordinates of each facial landmark.
-          [92.07, 119.49, -17.54],
-          [91.97, 102.52, -30.54],
-          ...
-        ],
-        scaledMesh: [ // The 3D coordinates of each facial landmark, normalized.
-          [322.32, 297.58, -17.54],
-          [322.18, 263.95, -30.54]
-        ],
-        annotations: { // Semantic groupings of the `scaledMesh` coordinates.
-          silhouette: [
-            [326.19, 124.72, -3.82],
-            [351.06, 126.30, -3.00],
-            ...
-          ],
-          ...
-        }
-      }
-    ]
-    */
-
-    // start canvas sketch
-      const settings = {
-        dimensions: [ 1080, 1080 ],
-      };
-
-    const sketch = ({ context, width, height }) => {
-
-      const agents = [];
-
-      for (let i = 0; i < predictions.length; i++) {
-        const keypoints = predictions[i].scaledMesh;
-
-          for (let i = 0; i < keypoints.length; i++) {
-          const [x, y, z] = keypoints[i];
-
-          // console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
-          agents.push(new Agent(x, y, z))
-        }
-      }
-
-      return ({ context, width, height }) => {
-        context.fillStyle = 'white';
-        context.fillRect(0, 0, width, height);
-
-        for (let i = 0; i < agents.length; i++) {
-          const agent = agents[i];
-
-            context.beginPath();
-            context.moveTo(agent.pos.x, agent.pos.y, agent.pos.z);
-            context.stroke();
-        }
-
-        console.log(agents)
-
-        agents.forEach(agent => {
-          agent.update();
-          agent.draw(context);
-        });
-      };
-    };
-      // plot points on a canvas
-      canvasSketch(sketch, settings);
-    }
-}
-
-class Vector {
-  constructor(x, y, z) {
-    this.x = x;
-    this.y = y;
-    this.z = z;
-  }
-}
-
-class Agent {
-  constructor(x, y, z) {
-    this.pos = new Vector(x, y, z);
-    this.radius = 1;
-  }
-
-  update() {
-    this.pos.x;
-    this.pos.y;
-    this.pos.z;
-  }
-
-  draw(context) {
-    context.save();
-    context.translate(this.pos.x, this.pos.y, this.pos.z);
-
-    context.lineWidth = 1;
-
-    context.beginPath();
-    context.arc(
-        0,
-        0,
-        this.radius,
-        0,
-        Math.PI * 2
-      );
-    context.fill();
-    context.stroke();
-
-    context.restore();
-  }
-}
-
-navigator.mediaDevices.getUserMedia({
-  video: {
-    width: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-    height: {
-      min: 720,
-      ideal: 1080,
-      max: 1440
-    },
-    facingMode: 'user'
-  }
-}).then( stream => {
-  /* use the stream */
-  video.srcObject = stream;
-  streamStarted = true;
-  console.log("stream", stream, "stream started", streamStarted)
-  generateMesh()
-}).catch( err => {
-  console.log(err)
+holistic.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  enableSegmentation: true,
+  smoothSegmentation: true,
+  refineFaceLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
 });
+
+holistic.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await holistic.send({image: videoElement});
+  },
+  width: 1280,
+  height: 3000
+});
+
+camera.start();
