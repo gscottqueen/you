fb2d74a38627c0b7e3b5e83379bcc944cb32c41c
gscottqueen <geoffreysqueen@gmail.com>
2022-04-07 11:07:16 -0400

    move everything to a useEffect and clear the rectangle

--- src/components/holistic.js
+++ src/components/holistic.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useRef, useState, useCallback } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Holistic } from "@mediapipe/holistic";
 import { Camera } from "@mediapipe/camera_utils";
 import {
@@ -13,110 +13,106 @@ import {
 const Runner = () => {
 const videoElement = useRef(null);
 const canvasElement = useRef(null);
-const [context, setContext] = useState(null)
-
-function onResults(results, canvasCtx = context) {
-  console.log('in component', results, canvasCtx)
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-    results.segmentationMask, 0, 0,
-    canvasElement.width,
-    canvasElement.height
-  );
-
-  // Only overwrite existing pixels.
-  canvasCtx.globalCompositeOperation = 'source-both';
-  canvasCtx.fillStyle = '#FFFFFF';
-  canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
-
-  // Only overwrite missing pixels.
-  canvasCtx.globalCompositeOperation = 'destination-atop';
-  canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
-
-  canvasCtx.globalCompositeOperation = 'source-over';
-
-  // pose-connections
-  drawConnectors(
-    canvasCtx,
-    results.poseLandmarks,
-    POSE_CONNECTIONS,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  // pose-landmarks
-  drawLandmarks(
-    canvasCtx,
-    results.poseLandmarks,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  // facemesh tesselation
-  drawConnectors(
-    canvasCtx,
-    results.faceLandmarks,
-    FACEMESH_TESSELATION,
-    {color: '#00000', lineWidth: 1}
-  );
-
-  // left hand connections
-  drawConnectors(
-    canvasCtx,
-    results.leftHandLandmarks,
-    HAND_CONNECTIONS,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  // left hand landmarks
-  drawLandmarks(
-    canvasCtx,
-    results.leftHandLandmarks,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  // right hand connections
-  drawConnectors(
-    canvasCtx,
-    results.rightHandLandmarks,
-    HAND_CONNECTIONS,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  // right hand landmarks
-  drawLandmarks(
-    canvasCtx,
-    results.rightHandLandmarks,
-    {color: '#000000', lineWidth: 1}
-  );
-
-  canvasCtx.restore();
-}
-
-const holistic = new Holistic({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
-}});
-
-console.log(holistic)
-
-holistic.setOptions({
-  modelComplexity: 1,
-  smoothLandmarks: true,
-  enableSegmentation: true,
-  smoothSegmentation: true,
-  refineFaceLandmarks: true,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
 
 useEffect(() => {
   const video = videoElement.current
   const canvas = canvasElement.current
-  setContext(canvas.getContext('2d'))
+  const context = canvas.getContext('2d')
+  const holistic = new Holistic({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
+  }});
 
-  console.log(context)
+  holistic.setOptions({
+    modelComplexity: 1,
+    smoothLandmarks: true,
+    enableSegmentation: true,
+    smoothSegmentation: true,
+    refineFaceLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+
+  function onResults(results, canvasCtx = context) {
+    console.log('in component', results, canvasCtx)
+    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+      results.segmentationMask, 0, 0,
+      canvasElement.width,
+      canvasElement.height
+    );
+
+    // Only overwrite existing pixels.
+    canvasCtx.globalCompositeOperation = 'source-both';
+    canvasCtx.fillStyle = '#FFFFFF';
+    canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+    // Only overwrite missing pixels.
+    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+
+    canvasCtx.globalCompositeOperation = 'source-over';
+
+    // pose-connections
+    drawConnectors(
+      canvasCtx,
+      results.poseLandmarks,
+      POSE_CONNECTIONS,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    // pose-landmarks
+    drawLandmarks(
+      canvasCtx,
+      results.poseLandmarks,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    // facemesh tesselation
+    drawConnectors(
+      canvasCtx,
+      results.faceLandmarks,
+      FACEMESH_TESSELATION,
+      {color: '#00000', lineWidth: 1}
+    );
+
+    // left hand connections
+    drawConnectors(
+      canvasCtx,
+      results.leftHandLandmarks,
+      HAND_CONNECTIONS,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    // left hand landmarks
+    drawLandmarks(
+      canvasCtx,
+      results.leftHandLandmarks,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    // right hand connections
+    drawConnectors(
+      canvasCtx,
+      results.rightHandLandmarks,
+      HAND_CONNECTIONS,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    // right hand landmarks
+    drawLandmarks(
+      canvasCtx,
+      results.rightHandLandmarks,
+      {color: '#000000', lineWidth: 1}
+    );
+
+    canvasCtx.restore();
+  }
 
   holistic.onResults(onResults)
+
   const camera = new Camera(video, {
     onFrame: async () => {
       await holistic.send({image: video});
@@ -126,7 +122,7 @@ useEffect(() => {
   });
 
   camera.start();
-}, [context, holistic, onResults])
+})
 
   return (
     <div className="container">
