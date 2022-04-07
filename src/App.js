74c8c39601b3b032c0771ed4dec30db3f126a9f7
gscottqueen <geoffreysqueen@gmail.com>
2022-04-07 11:29:08 -0400

    clean up, dir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Runner } from './components'
+import Runner from './Holistic'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{position: 'absolute'}}>you</h1>
           <Runner />
       </header>
     </div>
