268e623079ab7896ebde5801c85566e50c755181
gscottqueen <geoffreysqueen@gmail.com>
2022-04-14 16:41:02 -0400

    looping each outside coordinate to its matched band in index

--- src/Holistic.js
+++ src/Holistic.js
@@ -1,6 +1,17 @@
 import React, { useEffect, useRef } from 'react'
 import { Holistic } from "@mediapipe/holistic";
 import { Camera } from "@mediapipe/camera_utils";
+import * as CORDINATES from './coordinates'
+
+const {
+  outsideLeft,
+  outsideRight,
+  outsideCoordinates,
+  band10,
+  band109,
+  band93,
+  band132
+} = CORDINATES
 
 const Runner = () => {
 const videoElement = useRef(null);
@@ -27,51 +38,60 @@ useEffect(() => {
     minTrackingConfidence: 0.5
   });
 
-  const outsideContour = [
-    127, 162, 21, 54, 103, 67, 109, 10, 338, 297, 332, 284, 251, 389, 356, 454, 323, 361, 288, 397, 365, 379, 378, 400, 377, 152, 148, 176, 149, 150, 136, 172, 58, 132, 93, 234
+  const outsideContour = outsideLeft
+
+  const bands = [
+    band10,
+    band109,
+    band93,
+    band132
   ]
 
-  const band = [
-    132, 177, 147, 187, 205, 36, 142, 209, 198, 236, 3, 195, 248, 456, 420, 429, 371, 266, 425, 411, 376, 401, 361
-  ]
-
-  const outsideCoordinate = (height, fraction) => height / fraction
+  const outsideCoordinate = (size, fraction) => size / fraction
 
   const bodyCoordinates = canvas.getBoundingClientRect()
 
-  const drawContours = (context, results, canvas, outside) => {
-
+  const drawHorizontalContours = (context, results, band, canvas, outside, index) => {
     context.lineWidth = 1;
     context.strokeStyle = 'black';
-    context.beginPath();
-    context.moveTo(bodyCoordinates.left, outside)
 
 
-    for (let i = 0; i < band.length; i++) {
-      for (let j = 0; j < results.faceLandmarks.length; j++) {
-        if (j === band[i]) {
-          const x = results.faceLandmarks[j].x * canvas.width
-          const y = results.faceLandmarks[j].y * canvas.height
+    const drawLandMarkConnections = (r, index) => {
+      for (let l = 0; l < r.faceLandmarks.length; l++) {
+        if (l === index) {
+          const x = r.faceLandmarks[l].x * canvas.width
+          const y = r.faceLandmarks[l].y * canvas.height
           context.lineTo(x, y);
         }
       }
     }
-    context.lineTo(bodyCoordinates.right, outside)
-    context.stroke();
-};
+
+    const drawBand = (b, r) => {
+      for (let l = 0; l < b.length; l++) {
+        drawLandMarkConnections(r, b[l])
+      }
+    }
+
+    for (let j = 0; j < band.length; j++) {
+      context.beginPath()
+      context.moveTo(bodyCoordinates.left, outside)
+      drawBand(band, results)
+      context.lineTo(bodyCoordinates.right, outside)
+      context.stroke();
+      context.save()
+    }
+  };
 
   function onResults(results, canvasCtx = context) {
 
     if (results.ea) {
       canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
       canvasCtx.save();
-
-      // get coordinates from left, top - bottom, in fractions of array.length / 2
       let outside = 0;
 
-        for (let i = 0; i < outsideContour.length / 2; i++ ) {
+        for (let i = 0; i < bands.length; i++ ) {
           outside = outside + outsideCoordinate(bodyCoordinates.height, outsideContour.length / 2)
-          drawContours(canvasCtx, results, canvas, outside)
+          drawHorizontalContours(canvasCtx, results, bands[i], canvas, outside, i)
         }
 
       canvasCtx.restore();
